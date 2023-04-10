4e4bc76769d92ddc40074a320bde2249feb57418
bernardarhia <megamindcooperations@gmail.com>
2023-04-10 22:39:04 +0000

    added hover to dropdown links

--- src/App.vue
+++ src/App.vue
@@ -1,164 +1,235 @@
 <script setup>
-import {useRoute, useRouter} from "vue-router";
-import {onMounted, ref, watch} from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { onMounted, ref, watch } from "vue";
 
 const navbarCollapse = ref(null);
-const route =  useRoute();
+const route = useRoute();
 const router = useRouter();
 const moreServices = ref(null);
 
 const togglerElement = ref(null);
 
-
 const togglerFunction = (e) => {
-
-  if ( e.delegateTarget.classList.contains('collapsed')){
-    e.delegateTarget.children[0].style.display = 'block';
-    e.delegateTarget.children[1].style.display = 'none';
-  }else{
-    e.delegateTarget.children[0].style.display = 'none';
-    e.delegateTarget.children[1].style.display = 'block';
+  if (e.delegateTarget.classList.contains("collapsed")) {
+    e.delegateTarget.children[0].style.display = "block";
+    e.delegateTarget.children[1].style.display = "none";
+  } else {
+    e.delegateTarget.children[0].style.display = "none";
+    e.delegateTarget.children[1].style.display = "block";
   }
-
-}
+};
 
 const hideTimesIcon = () => {
-  togglerElement.value.children[0].style.display = 'block';
-  togglerElement.value.children[1].style.display = 'none';
-}
+  togglerElement.value.children[0].style.display = "block";
+  togglerElement.value.children[1].style.display = "none";
+};
 
-const goHome = () => {router.push({name: 'home'})}
+const goHome = () => {
+  router.push({ name: "home" });
+};
 
 onMounted(() => {
-
-  watch(() => route.name, () => {
-    navbarCollapse.value.classList.remove('show');
-    togglerElement.value.children[0].style.display = 'block';
-    togglerElement.value.children[1].style.display = 'none';
-  })
+  watch(
+    () => route.name,
+    () => {
+      navbarCollapse.value.classList.remove("show");
+      togglerElement.value.children[0].style.display = "block";
+      togglerElement.value.children[1].style.display = "none";
+    }
+  );
 
   // moreServices.value.onclick = () =>  navbarCollapse.value.classList.remove('show');
-
-
-})
+});
+const hover = ref(false)
+const programHover = ref(false)
 </script>
 
 <template>
-
   <div class="">
-  <nav class="navbar navbar-expand-lg fixed-top p-0 shadow shadow-sm" id="mainNav">
-    <div class="container-fluid">
-      <a class="navbar-brand" style="cursor: pointer;" @click="goHome">
-        <img src="/img/logo.jpg" class="logo" height="50" width="80" alt="logo"/>
-      </a>
+    <nav
+      class="navbar navbar-expand-lg fixed-top p-0 shadow shadow-sm"
+      id="mainNav"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand" style="cursor: pointer" @click="goHome">
+          <img
+            src="/img/logo.jpg"
+            class="logo"
+            height="50"
+            width="80"
+            alt="logo"
+          />
+        </a>
 
-      <a class="navbar-toggler text-decoration-none" @click="togglerFunction($event)" ref="togglerElement" data-bs-toggle="collapse"
-         data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
-         aria-expanded="false" aria-label="Toggle navigation" style="cursor: pointer;">
-        <span class="pi pi-bars ms-1"></span>
-        <span class="pi pi-times ms-1" style="display: none;"></span>
-      </a>
-      <div class="collapse navbar-collapse text-white" id="navbarResponsive" ref="navbarCollapse">
-        <ul class="navbar-nav text-uppercase ms-auto py-lg-0 fw-bold">
-          <li class="nav-item text-center">
-            <router-link :to="{name: 'home'}" class="nav-link text-decoration-none">home</router-link>
-          </li>
+        <a
+          class="navbar-toggler text-decoration-none"
+          @click="togglerFunction($event)"
+          ref="togglerElement"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style="cursor: pointer"
+        >
+          <span class="pi pi-bars ms-1"></span>
+          <span class="pi pi-times ms-1" style="display: none"></span>
+        </a>
+        <div
+          class="collapse navbar-collapse text-white"
+          id="navbarResponsive"
+          ref="navbarCollapse"
+        >
+          <ul class="navbar-nav text-uppercase ms-auto py-lg-0 fw-bold">
+            <li class="nav-item text-center">
+              <router-link
+                :to="{ name: 'home' }"
+                class="nav-link text-decoration-none"
+                >home</router-link
+              >
+            </li>
 
-          <li class="nav-item text-center">
-            <a href="#" class="nav-link text-decoration-none">product & services</a>
-          </li>
+            <li class="nav-item text-center">
+              <a href="#" class="nav-link text-decoration-none"
+                >product & services</a
+              >
+            </li>
 
-          <li class="nav-item text-center">
-            <a href="#" class="nav-link text-decoration-none">Social impact</a>
-          </li>
+            <li class="nav-item text-center">
+              <a href="#" class="nav-link text-decoration-none"
+                >Social impact</a
+              >
+            </li>
 
-          <li class="nav-item text-center">
-            <router-link :to="{name: 'gallery'}" class="nav-link text-decoration-none">gallery</router-link>
-          </li>
+            <li class="nav-item text-center">
+              <router-link
+                :to="{ name: 'gallery' }"
+                class="nav-link text-decoration-none"
+                >gallery</router-link
+              >
+            </li>
 
-          <li class="nav-item dropdown">
-            <div class="nav-link dropdown-toggle text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="d-lg-none">&nbsp; &nbsp; &nbsp;</span>our programs
-            </div>
-            <ul class="dropdown-menu text-center">
-              <li><a href="#" class="dropdown-item">project one</a></li>
-              <li><a href="#" class="dropdown-item">project two</a></li>
-            </ul>
-          </li>
+            <li class="nav-item dropdown">
+              <div
+                class="nav-link dropdown-toggle text-center"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                @mouseenter="programHover = true"
+                @mouseleave="programHover = false"
+              >
+                <span class="d-lg-none">&nbsp; &nbsp; &nbsp;</span>our programs
+              </div>
+              <ul class="dropdown-menu text-center" :class="{'show': programHover}">
+                <li><a href="#" class="dropdown-item" :class="{'show': programHover}">project one</a></li>
+                <li><a href="#" class="dropdown-item" :class="{'show': programHover}">project two</a></li>
+              </ul>
+            </li>
 
-          <li class="nav-item text-center">
-            <a href="#" class="nav-link text-decoration-none ">careers</a>
-          </li>
+            <li class="nav-item text-center">
+              <a href="#" class="nav-link text-decoration-none">careers</a>
+            </li>
 
-          <li class="nav-item dropdown">
-            <div class="nav-link dropdown-toggle text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="d-lg-none">&nbsp; &nbsp; &nbsp;</span>About
-            </div>
-            <ul class="dropdown-menu text-center">
-              <li><a href="/about" class="dropdown-item">Management</a></li>
-            </ul>
-          </li>
+            <li
+              class="nav-item dropdown"
+              @mouseover="hover = true"
+              @mouseleave="hover = false"
+         
+            >
+              <div
+                class="nav-link dropdown-toggle text-center"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                     :class="{'show': hover}"
+              >
+                <span class="d-lg-none">&nbsp; &nbsp; &nbsp;</span>About
+              </div>
+              <ul class="dropdown-menu text-center"      :class="{'show': hover}">
+                <li><a href="/about" class="dropdown-item">Management</a></li>
+              </ul>
+            </li>
 
-          <li class="nav-item text-center">
-            <a href="/contact" class="nav-link text-decoration-none">Contact</a>
-          </li>
-        </ul>
+            <li class="nav-item text-center">
+              <a href="/contact" class="nav-link text-decoration-none"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-
-  <router-view/>
+    <router-view />
 
     <footer class="bg-success text-white">
-    <div class="container-fluid py-5">
-      <div class="row text-center">
-        <div class="col-6 col-lg-3 mb-4">
-          <h4>Company</h4>
-            <a href="#" class="text-decoration-none text-white">About Us</a><br>
-            <a href="#" class="text-decoration-none text-white">Leadership</a><br>
-            <a href="#" class="text-decoration-none text-white">Impact</a><br>
-            <a href="#" class="text-decoration-none text-white">Careers</a><br>
-        </div>
+      <div class="container-fluid py-5">
+        <div class="row text-center">
+          <div class="col-6 col-lg-3 mb-4">
+            <h4>Company</h4>
+            <a href="#" class="text-decoration-none text-white">About Us</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white">Leadership</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white">Impact</a><br />
+            <a href="#" class="text-decoration-none text-white">Careers</a
+            ><br />
+          </div>
 
-        <div class="col-6 col-lg-3 mb-4">
-          <h4>Products</h4>
-          <a href="#" class="text-decoration-none text-white">Product One</a><br>
-          <a href="#" class="text-decoration-none text-white">Product Two</a><br>
-          <a href="#" class="text-decoration-none text-white">Product Three</a><br>
-        </div>
+          <div class="col-6 col-lg-3 mb-4">
+            <h4>Products</h4>
+            <a href="#" class="text-decoration-none text-white">Product One</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white">Product Two</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white">Product Three</a
+            ><br />
+          </div>
 
-        <div class="col-6 col-lg-3 mb-4">
-          <h4>Contact</h4>
-            <a href="#" class="text-decoration-none text-white"><span class="fw-bold">Phone:</span> 0243631511 / 0265557165 / 0202144534</a><br>
-            <a href="#" class="text-decoration-none text-white"><span class="fw-bold">Email:</span> hdj@gmail.com</a><br>
-            <a href="#" class="text-decoration-none text-white"><span class="fw-bold">Location:</span> Four 34 st Agric</a><br>
-        </div>
+          <div class="col-6 col-lg-3 mb-4">
+            <h4>Contact</h4>
+            <a href="#" class="text-decoration-none text-white"
+              ><span class="fw-bold">Phone:</span> 0243631511 / 0265557165 /
+              0202144534</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white"
+              ><span class="fw-bold">Email:</span> hdj@gmail.com</a
+            ><br />
+            <a href="#" class="text-decoration-none text-white"
+              ><span class="fw-bold">Location:</span> Four 34 st Agric</a
+            ><br />
+          </div>
 
-        <div class="col-6 col-lg-3 mb-4">
-          <h4>Connect</h4>
-          <div class="d-flex text-center">
-            <div class="mx-auto">
-              <a><span class="pi pi-facebook" style="font-size: 1.6em"></span></a>
-              <a ><span class="pi pi-linkedin mx-4" style="font-size: 1.6em"></span></a>
-              <a ><span class="pi pi-twitter" style="font-size: 1.6em"></span></a>
+          <div class="col-6 col-lg-3 mb-4">
+            <h4>Connect</h4>
+            <div class="d-flex text-center">
+              <div class="mx-auto">
+                <a
+                  ><span class="pi pi-facebook" style="font-size: 1.6em"></span
+                ></a>
+                <a
+                  ><span
+                    class="pi pi-linkedin mx-4"
+                    style="font-size: 1.6em"
+                  ></span
+                ></a>
+                <a
+                  ><span class="pi pi-twitter" style="font-size: 1.6em"></span
+                ></a>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
-    </div>
     </footer>
   </div>
 </template>
 
 <style scoped>
-
 .router-link-exact-active {
   color: gold;
 }
-.navbar-expand-lg{
-  background: linear-gradient(45deg, white, white, #eafcea );
+.navbar-expand-lg {
+  background: linear-gradient(45deg, white, white, #eafcea);
 }
-
 </style>
